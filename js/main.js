2e66a68feb302d53f9dd53065ffdea309f698dee
kirillye <kirill.stalker.2011@mail.ru>
2023-01-18 18:35:30 +0300

    revie 2.0

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 window.addEventListener("load", function () {
-  let body = document.querySelector(".body");
-  let btn = document.querySelector(".header__btn-edit");
+  let btn = document.querySelector(".person__btn-edit");
   let popop = document.querySelector(".popup");
   let btnClose = document.querySelector(".popup__btn-close");
   let formElement = document.querySelector(".popup__form");
   let nameInput = document.querySelector(".popup__input_content_name");
   let jobInput = document.querySelector(".popup__input_content_job");
-
-  btn.addEventListener("click", popopToggle);
-  btnClose.addEventListener("click", popopToggle);
+  let personTitle = document.querySelector(".person__title");
+  let personSubTitle = document.querySelector(".person__sub-title ");
 
   function popopToggle() {
     popop.classList.toggle("popup_opend");
-    body.classList.toggle("body_blocked");
+    nameInput.value = personTitle.textContent;
+    jobInput.value = personSubTitle.textContent;
   }
 
   function handleFormSubmit(evt) {
-    let headerTitle = document.querySelector(".header__title");
-    let headerSubTitle = document.querySelector(".header__sub-title");
     evt.preventDefault();
-    headerTitle.textContent = nameInput.value;
-    headerSubTitle.textContent = jobInput.value;
+    personTitle.textContent = nameInput.value;
+    personSubTitle.textContent = jobInput.value;
     popopToggle();
   }
 
+  btn.addEventListener("click", popopToggle);
+  btnClose.addEventListener("click", popopToggle);
   formElement.addEventListener("submit", handleFormSubmit);
 });
