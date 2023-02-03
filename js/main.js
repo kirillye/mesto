0e9d8032a7c0cd552ccd502782bdea51d39eeb85
kirillye <kirill.stalker.2011@mail.ru>
2023-02-03 12:36:44 +0300

    finishd

--- js/main.js
+++ js/main.js
@@ -34,15 +34,15 @@ window.addEventListener("load", function () {
 
   // popup image
   const popupImage = document.querySelector("#popup-image");
-  let imagePopupContent = popupImage.querySelector(".popup-image__img");
-  let imagePopupText = popupImage.querySelector(".popup__figcaption");
+  const imagePopupContent = popupImage.querySelector(".popup-image__img");
+  const imagePopupText = popupImage.querySelector(".popup__figcaption");
 
   // popup addArticle
   const popupAddArticle = document.querySelector("#popup-article");
   const btnAddArticle = document.querySelector(".person__add-article");
   const formAtricle = popupAddArticle.querySelector(".popup__form");
-  let articleTitle = popupAddArticle.querySelector("#popup__article-title");
-  let articleLinkImage = popupAddArticle.querySelector(
+  const articleTitle = popupAddArticle.querySelector("#popup__article-title");
+  const articleLinkImage = popupAddArticle.querySelector(
     "#popup__article-link-image"
   );
   const articleForm = popupAddArticle.querySelector(".popup__form");
@@ -50,44 +50,46 @@ window.addEventListener("load", function () {
   // popup editorNameUser
   const pupupEditorName = document.querySelector("#popup");
   const formElement = pupupEditorName.querySelector(".popup__form");
-  let nameInput = pupupEditorName.querySelector(".popup__input_content_name");
-  let jobInput = pupupEditorName.querySelector(".popup__input_content_job");
-  let personTitle = document.querySelector(".person__title");
-  let personSubTitle = document.querySelector(".person__sub-title ");
+  const nameInput = pupupEditorName.querySelector(".popup__input_content_name");
+  const jobInput = pupupEditorName.querySelector(".popup__input_content_job");
+  const personTitle = document.querySelector(".person__title");
+  const personSubTitle = document.querySelector(".person__sub-title ");
   const btnEditInfoPerson = document.querySelector(".person__btn-edit");
 
-  const btnsClose = document.querySelectorAll(".popup__btn-close");
+  const closeButtons = document.querySelectorAll(".popup__btn-close");
 
-  function popopToggle(popup) {
-    popup.classList.toggle("popup_opend");
+  closeButtons.forEach((button) => {
+    const popup = button.closest(".popup");
+    button.addEventListener("click", () => closePopup(popup));
+  });
+
+  function closePopup(popup) {
+    popup.classList.remove("popup_opend");
   }
 
-  function popupOpen(popup = pupupEditorName, img) {
-    if (popup == pupupEditorName) {
-      nameInput.value = personTitle.textContent;
-      jobInput.value = personSubTitle.textContent;
-    } else if (popup == popupImage) {
-      imagePopupContent.src = img.src;
-      imagePopupText.textContent = img.alt;
-    } else if (popup == popupAddArticle) {
-      articleTitle.value = "Название";
-      articleLinkImage.value = "Ссылка на картинку";
-    }
-    popopToggle(popup);
+  function openPopup(popup) {
+    popup.classList.add("popup_opend");
+  }
+
+  function renderPopupEditorName(popup) {
+    nameInput.value = personTitle.textContent;
+    jobInput.value = personSubTitle.textContent;
+
+    openPopup(popup);
   }
 
   function handleFormSubmit(evt) {
     evt.preventDefault();
     personTitle.textContent = nameInput.value;
     personSubTitle.textContent = jobInput.value;
-    popopToggle(pupupEditorName);
+    closePopup(pupupEditorName);
   }
 
   function addArticle(evt) {
     evt.preventDefault();
     let titleArticle = articleTitle.value;
     let imageSrc = articleLinkImage.value;
-    if (!imageSrc || "Ссылка на картинку") {
+    if (!imageSrc) {
       imageSrc =
         "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg";
     }
@@ -97,19 +99,22 @@ window.addEventListener("load", function () {
     });
 
     list.prepend(newArticle);
-    popopToggle(popupAddArticle);
+    evt.target.reset();
+    closePopup(popupAddArticle);
   }
 
   function createArticle(item) {
     const article = template.cloneNode(true);
     article.querySelector(".articles__title").textContent = item.name;
-    article.querySelector(".articles__image").alt = item.name;
+    if (!item.name) {
+      article.querySelector(".articles__image").alt = "Картинка статьи";
+    }
     article.querySelector(".articles__image").src = item.link;
 
     article
       .querySelector(".articles__image")
       .addEventListener("click", function () {
-        popupOpen(popupImage, this);
+        openPopup(popupImage, this);
       });
     article
       .querySelector(".articles__icon-trush")
@@ -133,18 +138,10 @@ window.addEventListener("load", function () {
     list.append(...cards);
   }
 
-  btnEditInfoPerson.addEventListener("click", () => popupOpen());
-  btnAddArticle.addEventListener("click", () => popupOpen(popupAddArticle));
-  btnsClose.forEach((item) => {
-    item.addEventListener("click", () => {
-      let popup = item.parentElement.parentElement;
-      if (popup.classList.contains("popup")) {
-        popopToggle(popup);
-      } else {
-        console.log("popup не найден");
-      }
-    });
-  });
+  btnEditInfoPerson.addEventListener("click", () =>
+    renderPopupEditorName(pupupEditorName)
+  );
+  btnAddArticle.addEventListener("click", () => openPopup(popupAddArticle));
   formElement.addEventListener("submit", handleFormSubmit);
   articleForm.addEventListener("submit", addArticle);
   renderCards();
